66c9c6279741682d5cccfa5fd86623c896638476
martijnhenriwoudstra@gmail.com <martijnhenriwoudstra@gmail.com>
2013-05-13 18:51:14 +0200

    Tutorial 10

--- Top_common/com/Martijn/Top/block/ModBlocks.java
+++ Top_common/com/Martijn/Top/block/ModBlocks.java
@@ -11,8 +11,11 @@ package com.Martijn.Top.block;
  */
 
 import net.minecraft.block.Block;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.item.ItemStack;
 
 import com.Martijn.Top.lib.BlockIds;
+import com.Martijn.Top.lib.Strings;
 
 import cpw.mods.fml.common.registry.GameRegistry;
 
@@ -23,6 +26,28 @@ public class ModBlocks {
 	public static void BlocksInit(){
 		NewBlock = new BlockNewBlock(BlockIds.NEWBLOCK_ID);
 
-		GameRegistry.registerBlock(NewBlock);
+		GameRegistry.registerBlock(NewBlock, Strings.NEWBLOCK_NAME);
+		
+		recipesInit();
+	}
+	
+	public static void recipesInit(){
+		
+		GameRegistry.addRecipe(new ItemStack(ModBlocks.NewBlock,3,0), new Object[]{
+			"DDD","SSS","DDD",'D', Block.dirt, 'S', Block.stone
+		});
+		
+		GameRegistry.addShapelessRecipe(new ItemStack(ModBlocks.NewBlock, 2,0), new Object[]{
+			new ItemStack(Block.dirt), new ItemStack(Block.dirt), new ItemStack(Block.dirt), new ItemStack(Block.stone)
+		});
+		
+		GameRegistry.addSmelting(Block.dirt.blockID, new ItemStack(ModBlocks.NewBlock,5,0), 1F);
+		
+		ItemStack enchant = new ItemStack(ModBlocks.NewBlock);
+		enchant.addEnchantment(Enchantment.fortune, 2);
+		
+		GameRegistry.addRecipe(enchant, new Object[]{
+				"DSD","DSD","DSD",'D', Block.dirt, 'S', Block.stone
+		});
 	}
 }
