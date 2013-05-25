589d7d704e7e019e2111e7cbb857dceaea798d87
martijnhenriwoudstra@gmail.com <martijnhenriwoudstra@gmail.com>
2013-05-25 22:37:21 +0200

    Tutorial 13

--- Top_common/com/Martijn/Top/Top.java
+++ Top_common/com/Martijn/Top/Top.java
@@ -11,6 +11,7 @@ import com.Martijn.Top.core.proxy.CommonProxy;
 import com.Martijn.Top.creativetab.TabTop;
 import com.Martijn.Top.item.ModItems;
 import com.Martijn.Top.lib.Reference;
+import com.Martijn.Top.world.generator.TopWorldGenerator;
 
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.Init;
@@ -21,6 +22,7 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
+import cpw.mods.fml.common.registry.GameRegistry;
 
 /**
  * Top
@@ -44,6 +46,8 @@ import cpw.mods.fml.common.network.NetworkMod;
 
 public class Top {
 	
+	TopWorldGenerator worldGenerator = new TopWorldGenerator();
+	
 	@SidedProxy(
 			clientSide = Reference.CLIENT_PROXY_LOCATION,
 			serverSide = Reference.COMMON_PROXY_LOCATION)
@@ -52,7 +56,7 @@ public class Top {
 	public static CreativeTabs TabTop = new TabTop(CreativeTabs.getNextID(), Reference.MOD_ID);
 	
 	@PreInit
-	public void preInit(FMLPreInitializationEvent event){
+	public void preInit(FMLPreInitializationEvent event){		
 		LocalizationHandler.loadLanguages();
 		
 		ConfigurationHander.init(new File(event.getModConfigurationDirectory().getAbsolutePath() + File.separator + Reference.CHANNEL_NAME + File.separator + Reference.MOD_ID + ".cfg"));
@@ -61,6 +65,7 @@ public class Top {
 		
 		ModItems.ItemInit();
 		
+		GameRegistry.registerWorldGenerator(worldGenerator);
 	}
 	@Init
 	public void init(FMLInitializationEvent event){
