a6ca96762e25e6ba6ff56742f1999d4b86bd3942
martijnhenriwoudstra@gmail.com <martijnhenriwoudstra@gmail.com>
2013-05-25 22:38:13 +0200

    Tutorial 13  (Missed one file)

---
+++ Top_common/com/Martijn/Top/world/generator/TopWorldGenerator.java
@@ -0,0 +1,73 @@
+package com.Martijn.Top.world.generator;
+
+import java.util.Random;
+
+import net.minecraft.block.Block;
+import net.minecraft.world.World;
+import net.minecraft.world.chunk.IChunkProvider;
+import net.minecraft.world.gen.feature.WorldGenMinable;
+
+import com.Martijn.Top.block.ModBlocks;
+
+import cpw.mods.fml.common.IWorldGenerator;
+
+public class TopWorldGenerator implements IWorldGenerator{
+
+	
+	@Override
+	public void generate(Random random, int chunkX, int chunkZ, World world, IChunkProvider chunkGenerator, IChunkProvider chunkProvider) {
+		switch(world.provider.dimensionId){
+		case 0:  generateOverworld   (world, random, chunkX * 16, chunkZ * 16);
+		case 1:  generateEnd		 (world, random, chunkX * 16, chunkZ * 16);
+		case -1: generateNether      (world, random, chunkX * 16, chunkZ * 16);
+		}
+		
+	}
+	
+	/**
+	 * 
+	 * This is where you add blocks which you want to spawn in the overworld. See example
+	 */
+	
+	public void generateOverworld (World world, Random random, int chunkX, int chunkZ){
+		this.spawnOres(ModBlocks.NewBlock, world, random, chunkX, chunkZ, 16, 16, 5, 50, 0, 50);
+		//this.spawnOres(ModBlocks.2ndBlock, world, random, chunkX, chunkZ, 16, 16, 5, 50, 0, 50);
+	}
+	
+	public void generateEnd (World world, Random random, int chunkX, int chunkZ){
+		
+	}
+
+	public void generateNether (World world, Random random, int chunkX, int chunkZ){
+	
+	}
+
+	/**
+	 * 
+	 * This Method adds ore generation. See below what all params mean
+	 * 
+	 * @param Block which you want to spawn
+	 * @param World where you want it to spawn
+	 * @param Randomizer used for spawning
+	 * @param Start of the Chunk in X-Direction
+	 * @param Start of the Chunk in Z-Direction
+	 * @param Max X-Size where the block can spawn, normal = 16
+	 * @param Max Z-Size where the block can spawn, normal = 16
+	 * @param The vain size
+	 * @param The chance to spawn a block
+	 * @param Minimum Y-level to spawn block
+	 * @param Maximum Y-level to spawn block
+	 * 
+	 */
+	
+	public void spawnOres(Block block, World world, Random random, int chunkX, int chunkZ, int XMax, int ZMax, int vainSize, int spawnChance, int YMin, int YMax){
+		for(int i = 0; i < spawnChance; i ++){
+			int posX = chunkX + random.nextInt(XMax);
+			int posZ = chunkZ + random.nextInt(ZMax);
+			int posY = YMin + random.nextInt(YMax-YMin);
+			(new WorldGenMinable(block.blockID, vainSize)).generate(world, random, posX, posY, posZ);
+		}
+	}
+	
+
+}
