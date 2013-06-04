50446480e40ca85681fbd5c40f48fce7df7b8e16
martijnhenriwoudstra@gmail.com <martijnhenriwoudstra@gmail.com>
2013-06-04 13:00:20 +0200

    Setup fixed

--- Top_common/com/Martijn/Top/item/ItemTop.java
+++ Top_common/com/Martijn/Top/item/ItemTop.java
@@ -5,18 +5,6 @@ import net.minecraft.item.Item;
 public class ItemTop extends Item{
 	public ItemTop(int id){
 		super(id-256);
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
 	}
 	
-	
-	@SideOnly(Side.CLIENT)
-	@Override
-	public void registerIcons(IconRegister register){
-		itemIcon = register.registerIcon(Reference.MOD_ID + ":" + this.getUnlocalizedName().substring(this.getUnlocalizedName().indexOf(".")+1));
-=======
->>>>>>> Tutorial 15
->>>>>>> 488eace9ef59fc06efca56ad694589af5e24c408
-	}
 }
